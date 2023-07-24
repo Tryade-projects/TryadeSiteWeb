6e327847f78bb8fd89f05881d3ce836ac348dfbd
DevWeb13 <giuliano.loic@sfr.fr>
2023-07-24 11:13:14 +0200

    feat: Create Button.jsx and settings Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { NavLink, useLocation } from "react-router-dom";
-
+import { NavLink } from 'react-router-dom';
+import Button from '../Button/Button';
 
 export default function Header() {
-    return (
-        <header>
-            <div>LOGO</div>
-            <nav>
-                <NavLink to="/">Accueil</NavLink>
-                <NavLink to="/rules">Règlements</NavLink>
-            </nav>
-            <div>BOUTon</div>
-        </header>
-    )
+  return (
+    <header>
+      <div>LOGO</div>
+      <nav>
+        <NavLink to='/'>Accueil</NavLink>
+        <NavLink to='/rules'>Règlements</NavLink>
+      </nav>
+      <div className='headerButtonsContainer'>
+        <Button
+          width={216}
+          height={51}
+          title='En ligne : 400/300'
+          borderColorClass='primaryColorBorder'
+        />
+        <Button
+          width={160}
+          height={51}
+          title='Jouer'
+          borderColorClass='secondaryColorBorder'
+          src='./assets/controller.svg'
+        />
+      </div>
+    </header>
+  );
 }
