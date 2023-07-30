bdfb50f9f713eff14ec8a4d770f725d116dab501
DevWeb13 <giuliano.loic@sfr.fr>
2023-07-30 17:34:52 +0200

    feat: Create Route and layout for <UpdatesPage />

--- src/components/Update/Update.jsx
+++ src/components/Update/Update.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Button from '../Button/Button'
+import React from 'react';
+import Button from '../Button/Button';
 
 const Update = () => {
-    return (
-        <div className='update'>
-            <div className="updateImg"><img src="./images/updateThumbnail.png" alt="" /></div>
-            <div className='updateUInfos'>
-                <h3 className='updateTitle'>Brothers & Hood</h3>
-                <h4 className='updateVersion'>V 1.6.15</h4>
-                <p className='updateText'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, dignissimos. Officiis tenetur aspernatur repellendus perspiciatis iure aliquam quaerat sunt provident repudiandae, laboriosam beatae velit consequatur asperiores numquam veritatis quis saepe.</p>
-            </div>
-            <Button 
-            title="LIRE"/>
-        </div>
-    )
-}
+  return (
+    <div className='update'>
+      <div className='updateImg'>
+        <img
+          src='/images/updateThumbnail.png'
+          alt=''
+        />
+      </div>
+      <div className='updateUInfos'>
+        <h3 className='updateTitle'>Brothers & Hood</h3>
+        <h4 className='updateVersion'>V 1.6.15</h4>
+        <p className='updateText'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam,
+          dignissimos. Officiis tenetur aspernatur repellendus perspiciatis iure
+          aliquam quaerat sunt provident repudiandae, laboriosam beatae velit
+          consequatur asperiores numquam veritatis quis saepe.
+        </p>
+      </div>
+      <Button title='LIRE' />
+    </div>
+  );
+};
 
-export default Update
+export default Update;
