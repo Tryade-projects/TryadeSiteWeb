ad406a35b0c7cab307b6f6930c04fc0e86d34408
DevWeb13 <giuliano.loic@sfr.fr>
2023-08-16 13:04:20 +0200

    feat: Add and display date for rules

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const Banner = ({ url }) => {
+/**
+ *
+ * @param {object} props
+ * @param {string} props.url - Url of the banner
+ * @param {Date} props.date - Date of the section
+ * @returns  {JSX.Element} - Rendered Banner component
+ */
+const Banner = ({ url, date }) => {
   return (
     <div className='banner'>
       <div
         className='imageBackground'
         style={{
           backgroundImage: `url(${url})`,
-        }}></div>
+        }}>
+        <div className='bannerTextContainer'>
+          {new Date(date).toLocaleDateString()}
+        </div>
+      </div>
     </div>
   );
 };
