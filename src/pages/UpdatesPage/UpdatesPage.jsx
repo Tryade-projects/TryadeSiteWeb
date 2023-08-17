145fb654b89ff5d7561ac31accb20f96f7d676aa
DevWeb13 <giuliano.loic@sfr.fr>
2023-08-17 17:44:05 +0200

    feat: Add pagination fetch data into updates page

--- src/pages/UpdatesPage/UpdatesPage.jsx
+++ src/pages/UpdatesPage/UpdatesPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Title from '../../components/Title/Title';
 import Button from '../../components/Button/Button';
 import Update from '../../components/Update/Update';
+import useUpdatesSectionQuery from '../../hooks/useUpdatesSectionQuery';
+import { useInView } from 'react-intersection-observer';
 
 const UpdatesPage = () => {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useUpdatesSectionQuery();
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
   return (
     <main className='page'>
       <div className='titleButtonContainer'>
@@ -11,19 +33,35 @@ const UpdatesPage = () => {
           mainTitle='Mises à jour'
           shadowTitle='DEVBLOG'
         />
-        <Button title='Voir tous nos mise à jour' />
+        <Button title='Voir toutes nos mise à jour' />
       </div>
       <section
         id='updates'
         className='sectionWrap'>
-        <Update />
-        <Update />
-        <Update />
-        <Update />
-        <Update />
-        <Update />
-        <Update />
+        {status === 'loading' ? (
+          <p>Chargement en cours...</p>
+        ) : status === 'error' ? (
+          <p>Erreur : Impossible de récupérer les données.</p>
+        ) : (
+          <>
+            {data?.pages.map((page, index) => (
+              <React.Fragment key={index}>
+                {page.map((section, sectionIndex) => (
+                  <Update
+                    key={`${index}-${sectionIndex}`}
+                    updateTitle={section.sectionTitle}
+                    updateVersion={section.version}
+                    updateText={section.details[0].content}
+                    updateThumbnail={section.urlBanner}
+                  />
+                ))}
+              </React.Fragment>
+            ))}
+          </>
+        )}
       </section>
+      <div ref={ref}></div>
+      {isFetching && !isFetchingNextPage && <div>Chargement...</div>}
     </main>
   );
 };
