726d241cc762ff76f162e1f102ba83a5666653a3
DevWeb13 <giuliano.loic@sfr.fr>
2023-08-21 10:40:46 +0200

    test: cfx.fetchServer() dont work??

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import useServerInfo from '../../utils/useServerInfo';
+import React, { useEffect } from 'react';
+// import useServerInfo from '../../utils/useServerInfo';
 import Button from '../Button/Button';
 import HeaderLogo from '../HeaderLogo/HeaderLogo';
 import HeaderBurger from '../HeaderBurger/HeaderBurger';
 import Navigation from '../Navigation/Navigation';
+import cfx from 'cfx-api';
 
 /**
  * @param {object} props
@@ -12,22 +13,30 @@ import Navigation from '../Navigation/Navigation';
  * @returns {JSX.Element}
  */
 export default function Header({ modalIsOpen, setModalIsOpen }) {
-  const serverInfo = useServerInfo();
-  console.log({ serverInfo });
+  // const serverInfo = useServerInfo();
+  // console.log({ serverInfo });
 
+  const fetchServerFn = async () => {
+    const server = await cfx.fetchServer('jpxoya');
+    console.log({ server });
+  };
+
+  useEffect(() => {
+    fetchServerFn();
+  }, []);
   return (
     <div className='headerContainer'>
       <header className='header'>
         <HeaderLogo />
         <Navigation />
         <div className='headerButtonsContainer'>
-          {serverInfo ? (
+          {/* {serverInfo ? (
             <div className='button primaryColorBorder'>
               <p className='buttonTitle'>{serverInfo.players.length}</p>
             </div>
           ) : (
             <p>Loading server info...</p>
-          )}
+          )} */}
 
           <Button
             title='Jouer'
